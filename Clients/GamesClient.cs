366cf32190867e64d04db580d65bc0a9ac1b8f7c
Prajwal <im.mht01@gmail.com>
2024-09-15 12:17:35 +0545

    added components

---
+++ Clients/GamesClient.cs
@@ -0,0 +1,37 @@
+﻿using GameStore.Frontend.Models;
+
+namespace GameStore.Frontend.Clients;
+
+public class GamesClient 
+{
+    private readonly List<GameSummary>games=
+    [
+         new(){
+            Id=1,
+            Name="Street Fighter",
+            Genre="Fighting",
+            Price= 19.9M,
+            ReleaseDate =new DateOnly(1992, 7, 15)
+
+        },
+         new(){
+             Id=2,
+            Name="Final Fantasy ii",
+            Genre="Roleplaying",
+            Price= 29.9M,
+            ReleaseDate =new DateOnly(2001, 7, 15)   
+
+        },
+         new(){
+            Id=3,
+            Name="Fifa",
+            Genre="Sports",
+            Price= 45.9M,
+            ReleaseDate =new DateOnly(2016, 7, 15)    
+
+        }
+         
+
+    ];
+    public GameSummary[] GetGames() => [.. games];
+}
